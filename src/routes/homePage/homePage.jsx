b1a5ae169d66558873954edb47bde37e82da5a58
Japheth Leung <japheth.leung@aucklandcouncil.govt.nz>
2024-10-11 19:29:53 +1300

    Test

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -11,7 +11,7 @@ function HomePage() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. 
+            aliquip ex ea commodo consequat.test
           </p>
           <SearchBar />
           <div className="boxes">
